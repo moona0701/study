822c5c3fbf02670caba8406317c547ae21c57298
moona0701 <moonayoung0701@naver.com>
2026-01-11 19:59:50 +0900

    Delete Java_Language/Projects directory

--- Java_Language/Projects/2_VocaMaster_Console/TestMain.java
+++
@@ -1,8 +0,0 @@
-package Project_ver1;
-
-public class TestMain {
-    public static void main(String[] args) {
-        VocManager manager = new VocManager("홍길동");
-        manager.makeVoc("res/words.txt");
-    }
-}
